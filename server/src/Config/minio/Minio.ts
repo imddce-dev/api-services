f619bf84e48659ae3c7e1a38e82d38b346d886b6
bereffer <sangphet1234@gmail.com>
2025-09-17 14:37:21 +0700

    dd

--- server/src/Config/minio/Minio.ts
+++ server/src/Config/minio/Minio.ts
@@ -1,7 +1,7 @@
 import { Client } from 'minio'
 
 export const minioClient = new Client({
-  endPoint: '192.168.110.7',  // IP ของ MinIO server
+  endPoint: '192.168.130.10',  // IP ของ MinIO server
   port: 9000,
   useSSL: false,              // true ถ้า MinIO ใช้ HTTPS
   accessKey: 'admin',

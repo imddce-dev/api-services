e8a06297f73fa04ddfa363e5b0a87e6ed179fd99
bereffer <sangphet1234@gmail.com>
2025-09-17 13:06:16 +0700

    dd

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-# entrypoint.sh
+# รอให้ MariaDB พร้อม
+while ! nc -z mariadb 3306; do
+  echo "Waiting for mariadb..."
+  sleep 1
+done
 
-# ทำ migration แบบ non-interactive ด้วย --force
+# ทำ migration แบบ non-interactive
 bun drizzle-kit push --force
 
 # เริ่ม server

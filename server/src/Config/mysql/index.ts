de61d2a86c6e3367f45c774bd7395764a3eb9988
bereffer <sangphet1234@gmail.com>
2025-09-17 12:29:06 +0700

    dd

--- server/src/Config/mysql/index.ts
+++ server/src/Config/mysql/index.ts
@@ -4,7 +4,8 @@ import * as schema from './schema';
 
 const poolConnection = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
+  // user: process.env.DB_USERNAME,
+  user: process.env.DB_USERNAME || process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: Number(process.env.DB_PORT || 3306),
